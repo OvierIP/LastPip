7b3017b2f95819a099e870afa0ac15aa50c54695
OvierI <111073908+OvierI@users.noreply.github.com>
2022-09-23 13:11:22 -0500

    Aquí cargamos todos los métodos

--- desoper/SecondPip.py
+++ desoper/SecondPip.py
@@ -28,41 +28,42 @@ def exponente1(m):
     return exp1
 
 def anomalyparimpar(n): #Este método halla una solución final Z para n par o impar; los ordena y los convierte en una tupla ascendente.
-    if n % 2 == 0:    
-    m=int(n/2-1)   
-    l1 = np.random.randint(1, 19, size=m)*exponente(m) 
-    k1 = np.random.randint(1, 19, size=m)*exponente(m) 
-    l2 = l1[:m] 
-    k2 = k1[:m]
-    anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
-    if 0 in anomaly.free.simplified: 
+  if n % 2 == 0:        
+      m=int(n/2-1)   
+      l1 = np.random.randint(1, 19, size=m)*exponente(m) 
+      k1 = np.random.randint(1, 19, size=m)*exponente(m) 
+      l2 = l1[:m] 
+      k2 = k1[:m]
+      anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
+      if 0 in anomaly.free.simplified:         
         return [None]
-    if max(abs(anomaly.free.simplified))>32:
+      if max(abs(anomaly.free.simplified))>30:
         return [None]
-    if anomaly.free.simplified[0]<0:
+      if anomaly.free.simplified[0]<0:
         anomaly.free.simplified = -anomaly.free.simplified
-    if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
+      if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
         return [None]
-    else:
+      else:
         return (np.sort(anomaly.free.simplified)).tolist()
 
-  else:   
-        m=int((n-3)/2)
-        l1 = np.random.randint(1, 19, size=m)*exponente(m) 
-        k1 = np.random.randint(1, 19, size=m+1)*exponente1(m) 
-        l2 = l1[:m] 
-        k2 = k1[:m+1]
-        anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
-        if 0 in anomaly.free.simplified:
-          return [None]
-        if anomaly.free.simplified[0]<0:
-          anomaly.free.simplified = -anomaly.free.simplified
-        if max(abs(anomaly.free.simplified))>32:
-          return [None]
-        if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
-          return [None]
-        else:
-          return (np.sort(anomaly.free.simplified)).tolist()
+  else: 
+    m=int((n-3)/2)
+    l1 = np.random.randint(1, 19, size=m)*exponente(m) 
+    k1 = np.random.randint(1, 19, size=m+1)*exponente1(m) 
+    l2 = l1[:m] 
+    k2 = k1[:m+1]
+    anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
+    if 0 in anomaly.free.simplified:
+      return [None]
+    if anomaly.free.simplified[0]<0:
+      anomaly.free.simplified = -anomaly.free.simplified
+    if max(abs(anomaly.free.simplified))>30:
+      return [None]
+    if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
+      return [None]
+    else:
+      return (np.sort(anomaly.free.simplified)).tolist()
+
 def all_sol_for_n(n):
   sol = [] 
   sol += [anomalyparimpar(n) for i in range(4000000)]
