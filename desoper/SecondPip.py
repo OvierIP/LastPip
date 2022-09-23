9d1e9b90ff4f2d68e731e5127a8ec5b90b296d24
OvierI <111073908+OvierI@users.noreply.github.com>
2022-09-23 12:45:37 -0500

    Update SecondPip.py

--- desoper/SecondPip.py
+++ desoper/SecondPip.py
@@ -20,33 +20,33 @@ def hello():
     return 'Hello, World!'
 
 def exponente(m):
-  exp=(-1)**np.random.randint(0,2, size=m)   #la función exponente evita soluciones triviales o ceros 
-  return exp
+    exp=(-1)**np.random.randint(0,2, size=m)   #la función exponente evita soluciones triviales o ceros 
+    return exp
 
 def exponente1(m):
-  exp1=(-1)**np.random.randint(0,2, size=m+1)
-  return exp1
+    exp1=(-1)**np.random.randint(0,2, size=m+1)
+    return exp1
 
 def anomalyparimpar(n): #Este método halla una solución final Z para n par o impar; los ordena y los convierte en una tupla ascendente.
-  if n % 2 == 0:    
-      m=int(n/2-1)   
-      l1 = np.random.randint(1, 19, size=m)*exponente(m) 
-      k1 = np.random.randint(1, 19, size=m)*exponente(m) 
-      l2 = l1[:m] 
-      k2 = k1[:m]
-      anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
-      if 0 in anomaly.free.simplified: 
+    if n % 2 == 0:    
+    m=int(n/2-1)   
+    l1 = np.random.randint(1, 19, size=m)*exponente(m) 
+    k1 = np.random.randint(1, 19, size=m)*exponente(m) 
+    l2 = l1[:m] 
+    k2 = k1[:m]
+    anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
+    if 0 in anomaly.free.simplified: 
         return [None]
-      if max(abs(anomaly.free.simplified))>32:
+    if max(abs(anomaly.free.simplified))>32:
         return [None]
-      if anomaly.free.simplified[0]<0:
+    if anomaly.free.simplified[0]<0:
         anomaly.free.simplified = -anomaly.free.simplified
-      if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
+    if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
         return [None]
-      else:
+    else:
         return (np.sort(anomaly.free.simplified)).tolist()
 
-  else: 
+  else:   
         m=int((n-3)/2)
         l1 = np.random.randint(1, 19, size=m)*exponente(m) 
         k1 = np.random.randint(1, 19, size=m+1)*exponente1(m) 
