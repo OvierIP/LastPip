b2e9d6d372e6ef664cddde9b711e0fb2c6459dd0
OvierI <111073908+OvierI@users.noreply.github.com>
2022-09-30 00:59:30 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,17 +1,17 @@
 import unittest
 import pandas as pd
-from desoper.SecondPip import principal 
+from desoper.SecondPip import solutionfinal 
 
 #test base
 
 
 class Test_hello(unittest.TestCase):
     def test__working1(self):
-        ls1=principal(5,9,0,30,400000)
+        ls1=solutionfinal(5,9,0,30,400000)
         self.assertEqual(12,ls1.shape[0], True)
 
     def test__working2(self):
-        ls2=principal(6,9,0,30,400000)
+        ls2=solutionfinal(6,9,0,30,400000)
         self.assertEqual(141,ls2.shape[0], True)
        
 
