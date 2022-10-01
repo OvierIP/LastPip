e15e0c83a200308a0cd5ad41c79a92b43d8b6b1a
OvierIP <114205293+OvierIP@users.noreply.github.com>
2022-10-01 16:48:47 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -6,13 +6,15 @@ from desoper.LastPip import solutionfinal
 
 
 class Test_hello(unittest.TestCase):
-    def test__working1(self):
-        ls1=solutionfinal(5)
-        self.assertEqual(11,ls1.shape[0], True)
+    
+    
+        def test_n5(self):
+                  sls_5 = solutionfinal(5)
+                  self.assertEqual(11, sls_5.shape[0], True)                                
 
-    def test__working2(self):
-        ls2=solutionfinal(6)
-        self.assertEqual(141,ls2.shape[0], True)
+        def test_n6(self):
+                  sls_6 = solutionfinal(6)
+                  self.assertEqual(112, sls_6.shape[0], True)
        
 
 if __name__ == '__main__':
