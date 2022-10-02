b2f469de85235837e68e9163e8afefefa73aab8c
OvierIP <114205293+OvierIP@users.noreply.github.com>
2022-10-02 10:28:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def main():
         name="LastPip",
 
         # Version number (initial):
-        version="0.0.3",
+        version="0.0.4",
 
         # Application author details:
         author="OvierIP",
