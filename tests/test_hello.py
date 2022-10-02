a2177e385d622d52b9210ca36b3f39b1d036ffa9
OvierIP <114205293+OvierIP@users.noreply.github.com>
2022-10-01 21:55:24 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -8,11 +8,11 @@ class Test_hello(unittest.TestCase):
      
     def test_n5(self):
         sls_5 = solutionfinal(5)
-        self.assertEqual(11, sls_5.shape[0])                                
+        self.assertEqual(11, sls_5.shape[0],True)                                
 
     def test_n6(self):
         sls_6 = solutionfinal(6)
-        self.assertEqual(112, sls_6.shape[0])
+        self.assertEqual(112, sls_6.shape[0],True)
        
 
 if __name__ == '__main__':
