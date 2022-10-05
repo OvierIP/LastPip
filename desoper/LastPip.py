8503fbd2e240e54ab27ff881071e14349321914b
OvierIP <114205293+OvierIP@users.noreply.github.com>
2022-10-05 05:57:49 -0500

    Update LastPip.py

--- desoper/LastPip.py
+++ desoper/LastPip.py
@@ -9,14 +9,31 @@ import time
 from itertools import permutations
 
 def exponente(m):
-  exp=(-1)**np.random.randint(0,2, size=m)   #la función exponente evita soluciones triviales o ceros 
+  
+  '''
+  la función exponente construye un arreglo de dimensión m
+  '''
+  
+  exp=(-1)**np.random.randint(0,2, size=m)   
   return exp
 
 def exponente1(m):
+  
+  '''
+  la función exponente1 construye un arreglo de dimensión m
+  '''
+  
   exp1=(-1)**np.random.randint(0,2, size=m+1)
   return exp1
 
-def anomalyparimpar(n): #Este método halla una solución final Z para n par o impar; los ordena y los convierte en una tupla ascendente.
+def anomalyparimpar(n): 
+  
+  ''' Esta función halla una solución final Z para un n par o impar;
+  la ordena de menor a mayor y la convierte en un diccionario de listas 
+  con claves l, k, z y gcd. Se verifica que no sea trivial, no se repita
+  ni que supere un zmax.
+  '''
+  
   if n % 2 == 0:        
 
     m=int(n/2-1)   
@@ -26,15 +43,15 @@ def anomalyparimpar(n): #Este método halla una solución final Z para n par o i
     k2 = k1[:m]
     anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
     if 0 in anomaly.free.simplified:         
-      return [None]
+      return {}
     if max(abs(anomaly.free.simplified))>30:
-      return [None]
+      return {}
     if anomaly.free.simplified[0]<0:
       anomaly.free.simplified = -anomaly.free.simplified
     if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
-      return [None]
+      return {}
     else:
-      return (np.sort(anomaly.free.simplified)).tolist()
+      return {"l":list(l2),"k":list(k2),"z":list(np.sort(anomaly.free.simplified)),"gcd":anomaly.free.gcd}
 
   else: 
     m=int((n-3)/2)
@@ -44,28 +61,37 @@ def anomalyparimpar(n): #Este método halla una solución final Z para n par o i
     k2 = k1[:m+1]
     anomaly.free(l2,k2), anomaly.free.gcd, anomaly.free.simplified
     if 0 in anomaly.free.simplified:
-      return [None]
+      return {}
     if anomaly.free.simplified[0]<0:
       anomaly.free.simplified = -anomaly.free.simplified
     if max(abs(anomaly.free.simplified))>30:
-      return [None]
+      return {}
     if ( 0 in [ sum(p) for p in itertools.permutations(anomaly.free.simplified, 2) ]): 
-      return [None]
+      return {}
     else:
-      return (np.sort(anomaly.free.simplified)).tolist()
+      return {"l":list(l2),"k":list(k2),"z":list(np.sort(anomaly.free.simplified)),"gcd":anomaly.free.gcd}
 
 def all_sol_for_n(n):
+  
+  ''' Esta función calcula el total de soluciones z para un n,
+  y en un espacio de parámetros determinado. Los presenta en
+  una lista de diccionarios
+  '''
+  
   sol = [] 
   sol += [anomalyparimpar(n) for i in range(500000)]
-  final = [i for i in sol if i[0] is not None]
-  return final
+  sls=[d for d in sol if d]  
+  return sls
 
 def solutionfinal(n):
+  ''' Esta función aplica multiprocessing para hallar las 
+  soluciones finales, no repetidas y las presenta en un
+  dataframe.
+  '''
+  
   from multiprocessing import Pool
   resultados = Pool().map(all_sol_for_n, list(range(n,n+1)))
-  df=pd.DataFrame(resultados)
-  df=df.T
+  df=pd.DataFrame(resultados[0])
   df=df.astype(str)
-  df=df.drop_duplicates().reset_index(drop=True)
-  df
+  df=df.drop_duplicates(subset=['z']).reset_index(drop=True)
   return df
